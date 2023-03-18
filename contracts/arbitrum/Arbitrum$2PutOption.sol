750f8895498da20c49961d76f2f38a5b21cf8f44
Sean <detectivekim1@gmail.com>
2023-03-18 19:39:33 +0900

    feat: deploy task

--- contracts/arbitrum/Arbitrum$2PutOption.sol
+++ contracts/arbitrum/Arbitrum$2PutOption.sol
@@ -5,16 +5,16 @@ pragma solidity ^0.8.0;
 
 import "../PutOptionToken.sol";
 
-contract Arbitrum$1PutOption is PutOptionToken {
-    constructor(uint256 strikePrice_)
+contract Arbitrum$2PutOption is PutOptionToken {
+    constructor()
         PutOptionToken(
-            0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8,
             0x912CE59144191C1204E64559FE8253a0e49E6548,
-            1,
+            0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8,
+            2 * 10**6,
             1679574903 + 24 * 60 * 60,
             10000, // 1%
-            "Arbitrum Put Option at $1",
-            "ARB$1PUT"
+            "Arbitrum Put Option at $2",
+            "ARB$2PUT"
         )
     {}
 }
